5a25d9ec8d484e30e8558e8755a160053eb272ec
NoblePrinceii <farhan.begg@students.makeschool.com>
2020-12-08 22:58:06 -0800

    update data chart

--- screens/DataScreen.js
+++ screens/DataScreen.js
@@ -2,12 +2,67 @@ import * as React from 'react';
 import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import {
+    LineChart,
+
+  } from 'react-native-chart-kit'
+  import { useSelector } from 'react-redux'
+
 
 export default function DataScreen({ navigation }) {
+    const workouts = useSelector((state) =>  state.workouts)
+    const data = workouts.map((item, index) => {
+        return{
+          name: item.name,
+     
+    
+        }
+      })
+
+      const chartConfig = {
+        backgroundColor: '#e26a00',
+        backgroundGradientFrom: '#fb8c00',
+        backgroundGradientTo: '#ffa726',
+        decimalPlaces: 2, // optional, defaults to 2dp
+        color: (opacity = .1) => `rgba(255, 255, 255, ${opacity})`,
+        style: {
+          borderRadius: 0
+        }
+      }
+
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Data Screen</Text>
-            <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+            <Text style={{ color: 'grey' , fontSize: 20}}>Data Screen</Text>
+            <LineChart
+                data={{
+                labels: [ 'Leg Press', 'Pull Ups', 'Push Ups', 'Sit Ups', 'Bench Press'],
+                datasets: [{
+                    data: [
+                    Math.random() * 10,
+                    Math.random() * 10,
+                    Math.random() * 10,
+                    Math.random() * 10,
+                    Math.random() * 10,
+                    Math.random() * 10,
+                 
+                    ]
+                }]
+                }}
+                width={380} // from react-native
+                height={220}
+                chartConfig={
+                chartConfig}
+                bezier
+                style={{
+                marginVertical: 10,
+                borderRadius: 5,
+                padding: 20
+                }}
+              />
+
+              <Button 
+              title="Go to Home" 
+              onPress={() => navigation.navigate('Home')} />
 
         </View>
     );
--- screens/WorkoutScreen.js
+++ screens/WorkoutScreen.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import { useState } from 'react';
 import { View, Text, Button, Picker, StyleSheet, TextInput, Input ,   ScrollView,  TouchableOpacity, FlatList, List,
-    Keyboard } from 'react-native';
+    Keyboard, } from 'react-native';
 import { useSelector } from 'react-redux'
 import { color } from 'react-native-reanimated';
 import {deleteWorkout} from '../actions'
@@ -9,18 +9,22 @@ import {useDispatch } from 'react-redux'
 
 
 
+
+
+
+
+
+
 export default function WorkoutScreen({ navigation }) {
 
     const workouts = useSelector((state) =>  state.workouts)
     const dispatch = useDispatch()
-
+    const [toggleCheckBox, setToggleCheckBox] = useState(false)
 
     const addWorkouts = workout => {
         workout.id = workouts.length + 1 
         setWorkouts([...workoutes, workout])
     }
-
-
 
         return (
             
@@ -56,6 +60,12 @@ export default function WorkoutScreen({ navigation }) {
             >
                 
             </Button>
+      
+  
+ 
+
+
+
 
 
         </View>
@@ -79,7 +89,13 @@ const styles = StyleSheet.create({
         fontSize: 20,
         color: 'grey',
         paddingTop: 5
-    }
+    },
+    checkbox: {
+        alignSelf: "center",
+        height: 50,
+        width: 50
+     
+      },
 
 
     //Check project repo for styles
